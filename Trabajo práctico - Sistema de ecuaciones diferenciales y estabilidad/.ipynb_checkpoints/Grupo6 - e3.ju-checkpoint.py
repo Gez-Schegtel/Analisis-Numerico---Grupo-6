8b03b4a9d43d2b5a617b1e4a2cfec6ee0d8bc223
Juani <gez.schegtel_ji@proton.me>
2025-10-27 22:24:18 -0300

    Empezamos la etapa 3.

---
+++ Trabajo práctico - Sistema de ecuaciones diferenciales y estabilidad/.ipynb_checkpoints/Grupo6 - e3.ju-checkpoint.py
@@ -0,0 +1,13 @@
+
+##
+"""
+# Título 1
+
+## Título 2
+
+Texto 
+
+"""
+
+## python 
+print("Hello dirty world")
---
+++ Trabajo práctico - Sistema de ecuaciones diferenciales y estabilidad/Grupo6 - e3.ju.py
@@ -0,0 +1,13 @@
+
+# %% [markdown]
+"""
+# Título 1
+
+## Título 2
+
+Texto 
+"""
+
+
+# %% python 
+print("Hello dirty world")
